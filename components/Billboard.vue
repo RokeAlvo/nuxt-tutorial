f9335d59eaea73844db53b92b1137656e24edfed
rokealvo <rokealvo@gmail.com>
2020-12-13 15:17:56 +0700

    added Billboard.vue

---
+++ components/Billboard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="billboard">
+    <img src="/girl-4898696_1920.jpg" alt="girl" class="billboard__image" />
+    <h1 class="billboard__title">Очень, очень важный заголовок</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Billboard',
+}
+</script>
+
+<style lang="scss">
+.billboard {
+  width: 100%;
+  height: 100vh;
+
+  &__image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &__title {
+    color: #fff;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
---
+++ components/PostsSlider.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="posts-slider">slider</div>
+</template>
+
+<script>
+export default {
+  name: 'PostsSlider',
+}
+</script>
+
+<style lang="scss"></style>
